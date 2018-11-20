9773ae80804a564561b6e54a4a097ea2f1817e77
Huaigu Lin <kevinlinhg@outlook.com>
2018-11-20 15:32:28 -0500

    testing post request on heroku

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const socketServer = require('http').Server(app);
 const io = require('socket.io')(socketServer);
 
 // create a GET route
+app.get('/', (req, res) => {
+  res.render('Why are you looking at this?');
+});
+
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
