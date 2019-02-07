b45f460eb12dac7d316260278b0b6d0854f5d603
Huaigu Lin <kevinlinhg@outlook.com>
2019-02-06 22:52:40 -0500

    formatting issue

--- palocal/src/PageHome.js
+++ palocal/src/PageHome.js
@@ -10,11 +10,7 @@ class PageHome extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-
-
   }
-
-
 
   handleChange(event) {
     this.setState({ userName: event.target.value });
@@ -29,22 +25,22 @@ class PageHome extends Component {
   }
 
   render() {
-    const responseGoogle = (response) => {
+    const responseGoogle = response => {
       console.log(response);
-    }
-    const googleID = (response) => {
+    };
+    const googleID = response => {
       console.log(response.googleId);
       this.props.socket.emit('login', response.googleId);
       this.props.history.push('/PagePDF');
       this.props.setUserName(response.googleId);
-    }
+    };
     return (
       <div>
-      <GoogleLogin
-        clientId="205354448545-sc5rs1bo1q8tcdg0crsfr8aiflgf54tp.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={googleID}
-        onFailure={responseGoogle}
+        <GoogleLogin
+          clientId="205354448545-sc5rs1bo1q8tcdg0crsfr8aiflgf54tp.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={googleID}
+          onFailure={responseGoogle}
         />
         <Navbar />
         <h1>This is the home page</h1>
