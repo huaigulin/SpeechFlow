4fd8ce6a893f281bfefbd7737bc7ad600a4f11c2
Huaigu Lin <kevinlinhg@outlook.com>
2019-03-15 14:47:52 -0400

    checkbox position and style

--- palocal/src/VideoThumbnail.js
+++ palocal/src/VideoThumbnail.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
+import { withStyles } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
 import './VideoThumbnail.css';
+
+const styles = {
+  root: {
+    color: grey[50]
+  }
+};
 
 class VideoThumbnail extends Component {
   constructor(props) {
@@ -8,9 +16,13 @@ class VideoThumbnail extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
       <div className="thumbnailDiv">
-        <Checkbox className="thumbnailCheckbox" />
+        <Checkbox
+          className="thumbnailCheckbox"
+          classes={{ root: classes.root, checked: classes.checked }}
+        />
         <img
           className="thumbnailImage"
           src={this.props.src}
@@ -21,4 +33,4 @@ class VideoThumbnail extends Component {
   }
 }
 
-export default VideoThumbnail;
+export default withStyles(styles)(VideoThumbnail);
