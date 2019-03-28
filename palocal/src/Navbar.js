901686c3c87d2200e270354f414b81b190edbe6b
Huaigu Lin <kevinlinhg@outlook.com>
2019-03-27 22:03:00 -0400

    small fix

--- palocal/src/Navbar.js
+++ palocal/src/Navbar.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { Link } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import Fab from '@material-ui/core/Fab';
 import Grid from '@material-ui/core/Grid';
@@ -15,12 +11,11 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import grey from '@material-ui/core/colors/grey';
 
-
 const styles = {
   fab: {
     backgroundColor: grey[200],
-    marginRight: 25,
-    marginTop: 10
+    marginRight: 25
+    // marginTop: 10
   },
   avatar: {
     // margin: 10,
@@ -28,19 +23,19 @@ const styles = {
     height: 30
   },
   root: {
-   flexGrow: 1,
+    flexGrow: 1
   },
   grow: {
-   flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
-   marginLeft: -12,
-   marginRight: 20,
- },
- beegButton: {
-   marginLeft: -12,
-   marginRight: 20,
-},
+    marginLeft: -12,
+    marginRight: 20
+  },
+  beegButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
 };
 
 class Navbar extends Component {
@@ -68,53 +63,71 @@ class Navbar extends Component {
 
     return (
       <div>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <IconButton className={classes.beegButton} href="PageMaterials" color="inherit">Upload Files</IconButton>
-          <IconButton className={classes.beegButton} href="PagePresentation" color="inherit">Presentation</IconButton>
-
-          {isLoggedIn ? (
-            <Grid container justify="flex-end" alignItems="center">
-              <Fab
-                aria-owns={open ? 'menu-appbar' : undefined}
-                aria-haspopup="true"
-                size="small"
-                onClick={this.handleAvatarClick}
-                className={classes.fab}
-              >
-                <Avatar
-                  alt="Profile image"
-                  src={this.props.profileImageUrl}
-                  className={classes.avatar}
-                />
-              </Fab>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right'
-                }}
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right'
-                }}
-                open={open}
-                onClose={this.handleClose}
-              >
-                <MenuItem onClick={this.handleSignOut}>Sign out</MenuItem>
-                {/* <MenuItem onClick={this.handleClose}>My account</MenuItem> */}
-              </Menu>
-            </Grid>
-          ) : (
-              <IconButton color="inherit" href="/">Login</IconButton>
-          )}
-        </Toolbar>
-      </AppBar>
-
+        <AppBar position="static">
+          <Toolbar>
+            {isLoggedIn ? (
+              <Grid container justify="space-between" alignItems="center">
+                <div>
+                  <IconButton
+                    className={classes.menuButton}
+                    color="inherit"
+                    aria-label="Menu"
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <IconButton
+                    className={classes.beegButton}
+                    href="PageMaterials"
+                    color="inherit"
+                  >
+                    Upload Files
+                  </IconButton>
+                  <IconButton
+                    className={classes.beegButton}
+                    href="PagePresentation"
+                    color="inherit"
+                  >
+                    Presentation
+                  </IconButton>
+                </div>
+                <Fab
+                  aria-owns={open ? 'menu-appbar' : undefined}
+                  aria-haspopup="true"
+                  size="small"
+                  onClick={this.handleAvatarClick}
+                  className={classes.fab}
+                >
+                  <Avatar
+                    alt="Profile image"
+                    src={this.props.profileImageUrl}
+                    className={classes.avatar}
+                  />
+                </Fab>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right'
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right'
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleSignOut}>Sign out</MenuItem>
+                  {/* <MenuItem onClick={this.handleClose}>My account</MenuItem> */}
+                </Menu>
+              </Grid>
+            ) : (
+              <IconButton color="inherit" href="/">
+                Home
+              </IconButton>
+            )}
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
