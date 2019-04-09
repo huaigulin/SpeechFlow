602f7b43e881b145f56ae34feb6fb9bbe3777de4
ditsky <34258091+ditsky@users.noreply.github.com>
2019-04-09 16:05:26 -0400

    thumbnails in video player

--- palocal/src/VideoList.js
+++ palocal/src/VideoList.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 
 class VideoList extends Component {
   render() {
+    const urls = []
+    const images = []
+    for (var i=0; i<this.props.videosList.length; i++){
+      var url = 'https://i1.ytimg.com/vi/' + this.props.videosList[i] + '/default.jpg';
+      images.push(
+        <img src={url} />
+      )
+    }
+
     return (
       <div>
         <h1> CURRENT VIDEOS: </h1>
-        <p> {this.props.videosList} </p>
+        {images}
       </div>
     );
   }
